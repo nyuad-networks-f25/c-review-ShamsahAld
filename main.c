8456c94d26e04dbaf9a0507dd3a897a63bbc165a
Shamsah Aldhanhani <shamsah@Shamsahs-MacBook-Air.local>
2025-09-01 10:24:17 +0400

    Include student.h into your makefile

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "status.h"
+#include "student.h"
 
 int main(){
+	student_t s;
+	s.name = "Shamsah";
 	printf("%d\n", ENROLLED);
+	printf("%s is %s\n", s.name, s.status ? "enrolled" : "not enrolled");
 	return 0;
 }
 
